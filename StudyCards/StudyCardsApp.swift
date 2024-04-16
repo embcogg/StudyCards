6ac02a0876fa2c1c279fecf382c06986983dd100
Emily Coggins <139669664+embcogg@users.noreply.github.com>
2024-04-16 17:09:36 -0500

    Initial Commit

---
+++ StudyCards/StudyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCardsApp.swift
+//  StudyCards
+//
+//  Created by Emily Coggins on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
